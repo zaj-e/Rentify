76e8315b5bfaf3bd80ef915e02b7ba338b11aaaa
Percy-Coding <55159839+Percy-Coding@users.noreply.github.com>
2020-11-05 07:17:09 -0500

    post request 1.0

--- lib/views/CreateOffice.dart
+++ lib/views/CreateOffice.dart
@@ -1,13 +1,34 @@
+import 'dart:convert';
+
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+import 'package:rentify/models/office.dart';
 
 class CreateOffice extends StatefulWidget {
   @override
   _CreateOfficeState createState() => _CreateOfficeState();
 }
-
+Future<Office> createOffice(String nombre,String descripcion,String correo,String telefono,String tarifa) async{
+  final String apiurl = 'rentifyAPI';
+  final response = await http.post(apiurl,body: {
+    "nombre":nombre,
+    "descripcion":descripcion,
+    "correo":correo,
+    "telefono":telefono,
+    "tarifa":tarifa
+  });
+  
+  return jsonDecode(response.body);
+}
 class _CreateOfficeState extends State<CreateOffice> {
   final formKey = GlobalKey<FormState>();
+  final TextEditingController nombreController = TextEditingController();
+  final TextEditingController descripcionController = TextEditingController();
+  final TextEditingController correoController = TextEditingController();
+  final TextEditingController telefonoController = TextEditingController();
+  final TextEditingController tarifaController = TextEditingController();
+
   @override
   Widget build(BuildContext context) {
     return WillPopScope(
@@ -30,7 +51,19 @@ class _CreateOfficeState extends State<CreateOffice> {
                 Align(
                   alignment: Alignment.topRight,
                   child: RaisedButton(
-                    onPressed: () {},
+                    onPressed: () {
+                      final String nombre = nombreController.text;
+                      final String descripcion = descripcionController.text;
+                      final String correo = correoController.text;
+                      final String telefono = telefonoController.text;
+                      final String tarifa = tarifaController.text;
+
+                      final Office oficina = await createOffice(nombre, descripcion, correo, telefono, tarifa);
+
+                      setState(() {
+
+                      });
+                    },
                     child: const Text('Publicar'),
                     color: Colors.blue,
                     textColor: Colors.white,
@@ -41,6 +74,7 @@ class _CreateOfficeState extends State<CreateOffice> {
                   height: 20,
                 ),
                 TextFormField(
+                  controller: nombreController,
                   decoration: const InputDecoration(hintText: 'Nombre'),
                   validator: (value) {
                     if (value.isEmpty) {
@@ -53,6 +87,7 @@ class _CreateOfficeState extends State<CreateOffice> {
                   height: 20,
                 ),
                 TextFormField(
+                  controller: descripcionController,
                   decoration: const InputDecoration(hintText: 'Descripcion'),
                   validator: (value) {
                     if (value.isEmpty) {
@@ -65,6 +100,7 @@ class _CreateOfficeState extends State<CreateOffice> {
                   height: 20,
                 ),
                 TextFormField(
+                  controller: correoController,
                   decoration: const InputDecoration(hintText: 'Correo'),
                   validator: (value) {
                     if (value.isEmpty) {
@@ -77,6 +113,7 @@ class _CreateOfficeState extends State<CreateOffice> {
                   height: 20,
                 ),
                 TextFormField(
+                  controller: telefonoController,
                   decoration: const InputDecoration(hintText: 'Telefono'),
                   validator: (value) {
                     if (value.isEmpty) {
@@ -89,6 +126,7 @@ class _CreateOfficeState extends State<CreateOffice> {
                   height: 20,
                 ),
                 TextFormField(
+                  controller: tarifaController,
                   decoration: const InputDecoration(hintText: 'Tarifa'),
                   validator: (value) {
                     if (value.isEmpty) {
